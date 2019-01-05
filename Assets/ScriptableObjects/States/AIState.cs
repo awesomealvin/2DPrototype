1e99bbe955a748aaf90781c286981c1caa9b0f2b
awesomealvin <fookinhellvin@outlook.com>
2019-01-05 23:02:33 +1300

    Created AI scripts

---
+++ Assets/ScriptableObjects/States/AIState.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AIState : ScriptableObject
+{
+    public abstract void Enter(AIStateController aiStateController);
+    public abstract void Execute(AIStateController aiStateController);
+    public abstract void Exit(AIStateController aiStateController);
+}
