407a0862ef07a4b0e4fe2b3488c528cd6660a112
awesomealvin <fookinhellvin@outlook.com>
2019-01-08 22:28:51 +1300

    Started projectiles

---
+++ Assets/ScriptableObjects/Weapons/RangeWeapon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangeWeapon : Weapon
+{
+    public float targetRange;
+
+    public GameObject projectile;
+
+    public float spread;
+
+    public override void Use(WeaponController weaponController)
+    {
+
+    }
+}
