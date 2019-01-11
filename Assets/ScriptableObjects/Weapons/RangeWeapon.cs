fcb42d05b7956bb58bda0e021d9b03b45c2a16b2
awesomealvin <fookinhellvin@outlook.com>
2019-01-12 00:30:58 +1300

    Range Weapon now shoots

--- Assets/ScriptableObjects/Weapons/RangeWeapon.cs
+++ Assets/ScriptableObjects/Weapons/RangeWeapon.cs
@@ -23,12 +23,17 @@ public class RangeWeapon : Weapon
         o.transform.position = weaponController.weaponPosition.position;
 
         // Shoot!
+        // Calculate spread
         float spreadValue = Random.Range(-spread, spread);
-
+        // Obtain weapon aim directio
         Vector2 aimDirection = weaponController.GetDirection();
-        Vector2 newDirection = Quaternion.AngleAxis(spreadValue, Vector3.forward) * aimDirection.normalized;
+        // Get the rotational offset using the spread value
+        Quaternion angleOffset = Quaternion.AngleAxis(spreadValue, Vector3.forward);
+        // Apply the angle offset to the aim direction
+        Vector2 direction = angleOffset * aimDirection.normalized;
 
-        o.rb.AddForce(newDirection * projectileSpeed, ForceMode2D.Impulse);
+        // Add force to the projectile
+        o.rb.AddForce(direction * projectileSpeed, ForceMode2D.Impulse);
 
     }
 }
