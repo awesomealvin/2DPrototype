50ecbc2ffa56c66f52c7056b211dde16ed12a239
awesomealvin <fookinhellvin@outlook.com>
2019-01-23 19:26:52 +1300

    Renamed folders

---
+++ Assets/ScriptableObjects/AI/EnemyStats.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "AI/ENemy Stats")]
+public class EnemyStats : ScriptableObject
+{
+    public int maxHealth;
+    public float movementSpeed;
+    public float attackRange;
+    public float disengageRange;
+}
