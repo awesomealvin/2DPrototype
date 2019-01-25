e07d2711906ff00ba2330f17f86cfcf37a8bff73
awesomealvin <fookinhellvin@outlook.com>
2019-01-25 14:08:32 +1300

    stufs

--- Assets/ScriptableObjects/Levels/Wave.cs
+++ Assets/ScriptableObjects/Levels/Wave.cs
@@ -20,10 +20,29 @@ public class Wave
 
     public void Initialise()
     {
-        int size = 0;
+        List<CircleController> enemiesList = new List<CircleController>();
+
         for (int i = 0; i < enemyTypes.Count; ++i)
         {
-            
+            for (int j = 0; j < enemyTypes[j].amount; ++j)
+            {
+                CircleController enemy = enemyTypes[j].enemyObjectPool.Obtain();
+                enemy.gameObject.SetActive(false);
+                enemiesList.Add(enemy);
+            }
+        }
+
+        ShuffleList(enemiesList);
+    }
+
+    private void ShuffleList(List<CircleController> list)
+    {
+        for (int i = list.Count- 1; i > 0; --i)
+        {
+            int random = Random.Range(0, i);
+            CircleController temp = list[i];
+            list[i] = list[random];
+            list[random] = temp;
         }
     }
 
